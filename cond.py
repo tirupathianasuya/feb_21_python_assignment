394e44461574e1199e3d3e5a84c1b9843c412ad4
tirupathianasuya <tirupathianasuya@gmail.com>
2025-02-27 02:02:17 +0530

    this is feb_21 assignment

---
+++ cond.py
@@ -0,0 +1,26 @@
+age = 18
+country = "USA"
+
+if age >= 18:
+    print("You are an adult.")
+else:
+    print("You are a minor.")
+
+if country == "USA":
+    print("You live in the United States.")
+elif country == "Canada":
+    print("You live in Canada.")
+else:
+    print("You live in another country.")
+
+temperature = 30
+if temperature > 25:
+    print("It's a hot day!")
+elif temperature > 15:
+    print("The weather is warm.")
+else:
+    print("It's cold outside.")
+
+is_raining = False
+if not is_raining:
+    print("You don't need an umbrella.")
